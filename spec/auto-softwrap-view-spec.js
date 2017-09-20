30991e810176f87caf21f2386867721797ccf79c
Max Schmidt <hackingmcs@gmail.com>
2017-09-20 18:45:36 +0200

    Initial commit

---
+++ spec/auto-softwrap-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AutoSoftwrapView from '../lib/auto-softwrap-view';
+
+describe('AutoSoftwrapView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
